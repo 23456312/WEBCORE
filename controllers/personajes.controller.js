62fe518fba6005ccfbe4ab1d35231e0a52450815
23456312 <A01707346@tec.mx>
2025-03-04 22:30:34 -0600

    lab14

--- controllers/personajes.controller.js
+++ controllers/personajes.controller.js
@@ -1,20 +1,31 @@
 const Personaje = require('../models/personaje.model');
 
 exports.get_agregar = (request, response, next) => {
-    response.render('agregar_personaje');
+    console.log(request.session);
+    response.render('agregar_personaje', {
+        isLoggedIn: request.session.isLoggedIn || false,
+        username: request.session.username || '',
+    });
 };
 
 exports.post_agregar = (request, response, next) => {
     console.log(request.body);
     const personaje = new Personaje(request.body.nombre);
     personaje.save();
+
+    response.setHeader('Set-Cookie', `ultimo_personaje=${personaje.nombre}`);
+
+
     console.log(Personaje.fetchAll());
     response.redirect('/personajes');
 };
 
-exports.get_lista = (request, response, next) => { 
+exports.get_lista = (request, response, next) => {
+    console.log(request.get('Cookie')); 
     response.render('lista_personajes', {
         personajes: Personaje.fetchAll(),
+        isLoggedIn: request.session.isLoggedIn || false,
+        username: request.session.username || '',
     });
 };
 
